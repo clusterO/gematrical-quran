8d0220d1fec53780761e62a0a46ff946eebb7a26
Fourmou <fourmou@outlook.fr>
2020-12-09 15:04:10 +0100

    Generate Godel Numbers

--- QGematria/Program.cs
+++ QGematria/Program.cs
@@ -7,7 +7,8 @@ namespace QGematria
     class Program
     {
         private static string GematricalQuran = "/home/c0/Projects/RiderProjects/QGematria/Quran/GematricalQuran.txt";
-        private static string QuranTXT = "/home/c0/Projects/RiderProjects/QGematria/Quran/quran-simple-clean.txt";
+        private static string QuranTXT = "/home/c0/Projects/RiderProjects/QGematria/Quran/quran.txt";
+        private static string GodelGematria = "/home/c0/Projects/RiderProjects/QGematria/Quran/GodelGematria.txt";
         private static Dictionary<char, int> GematricalValues = new Dictionary<char, int>
         {
             {'ا', 1},
@@ -47,31 +48,61 @@ namespace QGematria
             {'ي', 10},
             {'ئ', 1},
             {'ى', 1},
+            {'ّ', 0}
         };
+        private static int[] Primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        
         static void Main(string[] args)
         {
             using (TextReader Reader = File.OpenText(QuranTXT))
             {
-                string Line;
                 using (StreamWriter Writer = new StreamWriter(GematricalQuran))
                 {
-                    while ((Line = Reader.ReadLine()) != null)
+                    using (StreamWriter GWriter = new StreamWriter(GodelGematria))
                     {
-                        string NumericalLine = string.Empty;
-                        string[] Words = Line.Split(' ');
-                        foreach (string Word in Words)
+                        string Line = String.Empty;
+                        
+                        while ((Line = Reader.ReadLine()) != null)
                         {
-                            int Sum = 0;
-                            for (int i = 0; i < Word.Length; i++)
-                                Sum += GematricalValues[Word[i]];
+                            string NumericalLine = string.Empty;
+                            string[] Words = Line.Split(' ');
 
-                            NumericalLine += Sum + " ";
+                            foreach (string Word in Words)
+                            {
+                                int Sum = 0;
+                                for (int i = 0; i < Word.Length; i++)
+                                {
+                                    // Add Special Cases
+                                    if (GematricalValues.ContainsKey(Word[i]))
+                                        Sum += GematricalValues[Word[i]];
+                                }
+
+                                NumericalLine += Sum + " ";
+                            }
+                            
+                            Writer.WriteLine(NumericalLine);
+                            GWriter.WriteLine(GNumberIt(NumericalLine));
                         }
-
-                        Writer.WriteLine(NumericalLine);
                     }
                 }
             }
+        }
+
+        private static string GNumberIt(string Sentence)
+        {
+            string[] Words = Sentence.Split(' ');
+            string GSentence = string.Empty;
+            
+            foreach (string Word in Words)
+            {
+                int GWord = 0;
+                for (int i = 0; i < Word.Length; i++)
+                    GWord *= Primes[i]^Word[i];
+
+                GSentence += GWord + " ";
+            }
+
+            return GSentence;
         }
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,30 @@
 [_] Map Words to Emotions
 
 [_] Map Phrases to Subjects
+
+#### Godel Numbering
+The number might be a concatenation instead of the actual method (Sum)
+
+- ASCII number (000-127)
+- Pad to 3 digits
+- Concatenate
+
+Godel used a system based on prime factorization
+
+1- Map Symbols to Natural numbers
+
+0 - 1 || => - 3 || ( - 5 ||  ) - 7
+
+###### Godel encoding of a seqence is the product of the first n prime to the corespending values in the sequence
+enc(x1, x2, x3, ..., xn) = 2^x1 . 3^x2 . 5^x3. ... . Pn^xn
+
+A number obtained in this way can be uniquely factorized into prime factors
+###### Godel used this scheme at two levels:
+> - To encode sequences of symbols representing formulae
+> - To encode sequences of formulae representing proofs
+
+This allows to show correspondance between statements
+
+#### Other Formulations : 
+- Nagel and newman encoding
+
